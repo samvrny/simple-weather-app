9d118ad013f2fd2751ebb46c1bd95eca7b1f7d20
samvrny <samvrny@gmail.com>
2022-06-30 17:12:48 -0500

    retrieved latitude data from the api

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,26 +10,39 @@ var clickHandler = function(event) {
 
     if (city) {
         getNewCityLocation(city);
-        weatherFormInput.textContent = "";
+        weatherFormInput.value = "";
     }
     else {
         alert("You must enter in a valid city!");
     }
 };
 
+//This function grabs the latitude and longitude from a geo API to use to grab weather data
 var getNewCityLocation = function(city) {
     var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=0743f583ed199b32639e47dedaa949e4";
 
     fetch(geoApiUrl).then(function(response) {
 
         if(response.ok) {
-            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].lat) {
+                        var lat = data[i].lat;
+                        console.log(lat);
+                    }
+                }
+            });
         }
         else {
             alert("Something went wrong");
         }
-    })
+    });
 
 };
+
+var getTheWeather = function(lat, lon) {
+    console.log(lat, lon);
+}
 
 weatherFormEl.addEventListener("submit", clickHandler);
