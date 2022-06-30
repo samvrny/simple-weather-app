ac057f047ecfc8c5762af6bfa1003d3c4abaf807
samvrny <samvrny@gmail.com>
2022-06-30 16:26:24 -0500

    got the geolocation api to work somehow

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,35 @@
 var weatherFormEl = document.querySelector("#city-search");
 var weatherFormInput = document.querySelector("#city");
 
-var newCity = function(event) {
+//This function takes in the new city name and sends it to be processed
+var clickHandler = function(event) {
     event.preventDefault();
-    console.log("Hello World");
+
+    var city = weatherFormInput.value.trim();
+    console.log(city);
+
+    if (city) {
+        getNewCityLocation(city);
+        weatherFormInput.textContent = "";
+    }
+    else {
+        alert("You must enter in a valid city!");
+    }
 };
 
-weatherFormEl.addEventListener("submit", newCity);
+var getNewCityLocation = function(city) {
+    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=0743f583ed199b32639e47dedaa949e4";
+
+    fetch(geoApiUrl).then(function(response) {
+
+        if(response.ok) {
+            console.log(response);
+        }
+        else {
+            alert("Something went wrong");
+        }
+    })
+
+};
+
+weatherFormEl.addEventListener("submit", clickHandler);
