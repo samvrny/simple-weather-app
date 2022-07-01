436a72c7fae6248032980a51a357f48f5817bc25
samvrny <samvrny@gmail.com>
2022-07-01 13:38:48 -0500

    got the current cities weather to start printing

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,25 +1,32 @@
 var weatherFormEl = document.querySelector("#city-search");
 var weatherFormInput = document.querySelector("#city");
+var rightSide = document.querySelector("#right-side");
+var currentCity = document.getElementById("current-city");
 var lat;
 var lon;
 var city;
 var todaysTemp;
-var temp = [];
+//var temp = [];
+var todaysIcon;
+var todaysWind;
+var todaysUv;
+var todaysHumidity;
+
 
 //This function takes in the new city name and sends it to be processed
 var clickHandler = function(event) {
     event.preventDefault();
     lat = "";
     lon = "";
-    temp = [];
-    todaysTemp = "";
+    //temp = [];
+    //todaysTemp = "";
 
     city = weatherFormInput.value.trim();
     console.log(city);
 
     if (city) {
         getNewCityLocation(city);
-        weatherFormInput.value = "";
+        weatherFormInput.value = ""; //will need to change this later so that the city shows up for the weather input
     }
     else {
         alert("You must enter in a valid city!");
@@ -63,23 +70,46 @@ var getTheWeather = function() {
     fetch(weatherApiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data.current.temp);
+                console.log(data);
 
                 if (data.current.temp) {
                     todaysTemp = data.current.temp;
-                    console.log(todaysTemp);
+                    console.log(todaysTemp); //cosolelog
                 }
+                //console.log(data.current.weather[0].icon);
+                if (data.current.weather[0].icon) {
+                    todaysIcon = data.current.weather[0].icon;
+                }
+                console.log(todaysIcon);
 
-                // for (var i = 0; i < data.length; i++) {
-                //     if (data.current.temp) {
-                //         todaysTemp = data.current.temp;
-                //         console.log(todaysTemp);
-                //     } //This is where im stuck. Need to figrue out how to extract the data from this array.
-                // } console.log(todaysTemp);
+                printTheWeatherCurrent();
             });
         }
     });
-    
+};
+
+var printTheWeatherCurrent = function() {
+    console.log("hello world");
+
+    rightSide.classList.remove("hide");
+    while (currentCity.firstChild) { //This removes the old weather data
+        currentCity.removeChild(currentCity.firstChild);
+    }
+
+    var currentCityName = document.createElement("h3")
+    currentCityName.textContent = city;
+    currentCity.appendChild(currentCityName);
+
+    var currentCityTemp = document.createElement("p");
+    currentCityTemp.textContent = todaysTemp + " Degrees.";
+    currentCity.appendChild(currentCityTemp);
+
+    var currentCityIcon = document.createElement("img")
+    currentCityIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + todaysIcon + "@2x.png");
+    currentCity.appendChild(currentCityIcon);
+
+
 }
 
+//need the city name, the current date, the icon for weather, the temp, humidity, wind speed, UV index
 weatherFormEl.addEventListener("submit", clickHandler);
