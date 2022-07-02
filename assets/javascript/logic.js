450958645d2cd663fece54933e42dcf88e8a0470
samvrny <samvrny@gmail.com>
2022-07-02 13:35:34 -0500

    did some work during office hours and got my basic cards set up

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,15 +2,16 @@ var weatherFormEl = document.querySelector("#city-search");
 var weatherFormInput = document.querySelector("#city");
 var rightSide = document.querySelector("#right-side");
 var currentCity = document.getElementById("current-city");
+var cityCards = document.getElementById("city-cards");
 var lat;
 var lon;
 var city;
 var todaysTemp;
-//var temp = [];
 var todaysIcon;
 var todaysWind;
 var todaysUv;
 var todaysHumidity;
+var fiveDayWeather = [];
 
 
 //This function takes in the new city name and sends it to be processed
@@ -18,7 +19,6 @@ var clickHandler = function(event) {
     event.preventDefault();
     lat = "";
     lon = "";
-    //temp = [];
 
     city = weatherFormInput.value.trim();
     console.log(city);
@@ -36,12 +36,12 @@ var clickHandler = function(event) {
 var getNewCityLocation = function(city) {
     //later will add another search field/OR a dropdown menu to chose locations from
     var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=0743f583ed199b32639e47dedaa949e4";
-
+    
     fetch(geoApiUrl).then(function(response) {
 
         if(response.ok) {
             response.json().then(function(data) {
-                //console.log(data);
+                console.log(data);
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].lat) {
                         lat = data[i].lat;
@@ -62,7 +62,7 @@ var getNewCityLocation = function(city) {
 };
 
 var getTheWeather = function() {
-    //console.log(lat, lon, "yay");
+    console.log(lat, lon, "yay"); //consolelog
 
     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&units=imperial&appid=0743f583ed199b32639e47dedaa949e4";
     
@@ -71,27 +71,35 @@ var getTheWeather = function() {
             response.json().then(function(data) {
                 console.log(data);
 
+                //these if statements set the current weather to variables to print
                 if (data.current.temp) {
                     todaysTemp = data.current.temp;
-                    console.log(todaysTemp, "temp"); //cosolelog
+                    //console.log(todaysTemp, "temp"); //cosolelog
                 }
                 if (data.current.weather[0].icon) {
                     todaysIcon = data.current.weather[0].icon;
                 }
-                console.log(todaysIcon, "icon"); //console.log
+                //console.log(todaysIcon, "icon"); //console.log
                 if (data.current.wind_speed) {
                     todaysWind = data.current.wind_speed;
-                    console.log(todaysWind, "wind"); //console.log
+                    //console.log(todaysWind, "wind"); //console.log
                 }
                 if(data.current.humidity) {
                     todaysHumidity = data.current.humidity;
-                    console.log(todaysHumidity, "humidity"); //console.log
+                    //console.log(todaysHumidity, "humidity"); //console.log
                 }
                 if (data.current.uvi) {
                     todaysUv = data.current.uvi;
-                    console.log(todaysUv, "uvi"); //console.log
+                    //console.log(todaysUv, "uvi"); //console.log
                 }
-                printTheWeatherCurrent();
+
+                //This sets the daily weather into a variable to use
+                if (data.daily) {
+                    fiveDayWeather = data.daily;
+                    //console.log(fiveDayWeather, "Meow");
+                }
+                printTheWeatherCurrent(); //call to current weather display
+                removeCards(); //call to future weather display
             });
         }
     });
@@ -127,6 +135,58 @@ var printTheWeatherCurrent = function() {
     var currentCityWind = document.createElement("p");
     currentCityWind.textContent = "Wind Speed: " + todaysWind + " M.P.H.";
     currentCity.appendChild(currentCityWind);
+};
+
+var removeCards = function() {
+
+    while(cityCards.firstChild) {
+        cityCards.removeChild(cityCards.firstChild);
+    }
+
+    futureWeatherCards();
+};
+
+var futureWeatherCards = function() {
+    //console.log(fiveDayWeather, "Moo");
+    for (var i = 0; i < 5; i++) {
+
+        var dailyIcon = fiveDayWeather[i].weather[0].icon;
+        //console.log(dailyIcon);
+
+        var dailyTemp = fiveDayWeather[i].temp.day;
+        //console.log(dailyTemp);
+
+        var dailyHumidity = fiveDayWeather[i].humidity;
+        //console.log(dailyHumidity);
+
+        var dailyWind = fiveDayWeather[i].wind_speed;
+        //console.log(dailyWind, "Wind");
+
+        var futureWeatherCard = document.createElement("div");
+        futureWeatherCard.classList.add("card");
+        cityCards.appendChild(futureWeatherCard);
+
+        var newCard = document.createElement("div")
+        newCard.classList.add("card-body");
+        futureWeatherCard.appendChild(newCard)
+
+        var futureCity = document.createElement("h3");
+        futureCity.textContent = city;
+        newCard.appendChild(futureCity);
+
+        var futureIcon = document.createElement("img");
+        futureIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + dailyIcon + "@2x.png")
+        newCard.appendChild(futureIcon);
+
+        var futureTemp = document.createElement("p");
+        futureTemp.textContent = "Temp: " + dailyTemp + " F";
+        newCard.appendChild(futureTemp);
+
+        var futureHumidity = document.createElement("p");
+        futureHumidity.textContent = "Humidity: " + dailyHumidity + " %";
+        newCard.appendChild(futureHumidity);
+    }
+    //needs date, icon, temp, wind speed, humidity
 };
 
 
