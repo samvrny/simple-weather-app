c845d59e5f68e08b4e60fc810955d2586b340a7c
samvrny <samvrny@gmail.com>
2022-11-22 17:13:18 -0600

    fixed displays for printing the weather (Case Issue)

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -127,7 +127,7 @@ var printTheWeatherCurrent = function() {
 
     var currentCityName = document.createElement("h2")
     currentCityName.classList.add("text-light");
-    currentCityName.textContent = "Current Weather For " + city + " " + currentDate;
+    currentCityName.textContent = "Current Weather For " + city.charAt(0).toUpperCase() + city.slice(1).toLowerCase() + " " + currentDate;
     currentCity.appendChild(currentCityName);
 
     var currentCityIcon = document.createElement("img");
@@ -193,7 +193,7 @@ var futureWeatherCards = function() {
         cityCards.appendChild(futureWeatherCard);
 
         var futureCity = document.createElement("div");
-        futureCity.textContent = city + " " + fiveDayDate;
+        futureCity.textContent = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase() + " " + fiveDayDate;
         futureCity.classList.add("card-header");
         futureWeatherCard.appendChild(futureCity);
 
